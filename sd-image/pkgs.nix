0e5d0beb81f94392a54ae16f94a940c84214e9a1
Joe Hermaszewski <git@monoid.al>
2019-08-18 23:15:10 +0800

    Bump to nixos unstable

--- sd-image/pkgs.nix
+++ sd-image/pkgs.nix
@@ -1,6 +1,6 @@
-let pkgsRev = "4a08e95238996b1e7afc4a1491ac3f0a5181f597";
-    pkgsSHA256 = "062n0hqfmnmn39zakmbb4lbx4h5yk7nbzh7l75j6rggpmq7130hf";
+let pkgsRev = "5b534244153d1d8aa134c57d58e27f3c74110ead";
+    pkgsSHA256 = "1px2zdvc60ljyi9ln6ypplphrmsmhii0hk0rah3dxzp9ycd8ralq";
 in builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/${pkgsRev}.tar.gz";
+  url = "https://github.com/expipiplus1/nixpkgs/archive/${pkgsRev}.tar.gz";
   sha256 = pkgsSHA256;
 }
